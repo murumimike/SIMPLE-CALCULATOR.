f4e76d366a53dea7058207a3102cd683888d198e
murumimike <106368800+murumimike@users.noreply.github.com>
2022-07-11 15:36:30 +0300

    Add files via upload

---
+++ simple calc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*simple calculator
+created by michael murimi
+on july 11 2022
+c89 compiler
+MIT Licence*/
+
+int main()
+{int num1,num2,sum,diff,product;
+float quotient
+printf("\tsimple calculator\n")
+printf("enter two numbers:");
+scanf("%d%d",&num1,&num2);
+
+diff=num1+num2;
+diff=num-num2;
+product=num1*num2;
+quotient=(foat)num1/num2;
+
+//output
+printf("%d+%d=%d\n",num1,num2,sum);
+printf("%d-%d=%d\n",num1,num2,diff);
+printf("%d*%d=%f\n",num1,num2,quotient);
